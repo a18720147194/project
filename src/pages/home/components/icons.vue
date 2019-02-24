121da95932e33958e3059c93da0d15f6f276031b
18720147194 <44021013+18720147194@users.noreply.github.com>
2019-02-24 12:18:33 +0800

    add recommend

--- src/pages/home/components/icons.vue
+++ src/pages/home/components/icons.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="icons">
-    <swiper :options='swiperOption'>
+    <swiper>
       <swiper-slide v-for='(page,index) of pages' :key='index'>
         <div class="icon" v-for='item of page' :key='item.id'>
           <div class="icon-img">
@@ -9,7 +9,6 @@
           <p class="icon-desc">{{item.desc}}</p>
         </div>
       </swiper-slide>
-      <div class="swiper-pagination"  slot="pagination"></div>
     </swiper>
   </div>
 </template>
@@ -40,24 +39,24 @@ export default {
           desc: '动植园'
         },
         {
-          id: '0005',
+          id: '0001',
           imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
           desc: '景点门票'
         },
         {
-          id: '0006',
-          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
-          desc: '景点门票'
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png',
+          desc: '滑雪季'
         },
         {
-          id: '0007',
-          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
-          desc: '景点门票'
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1710/a6/83f636bd75ae6302.png',
+          desc: '泡温泉'
         },
         {
-          id: '0008',
-          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
-          desc: '景点门票'
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png',
+          desc: '动植园'
         },
         {
           id: '0009',
@@ -88,33 +87,35 @@ export default {
   .icons >>> .swiper-container
     height: 0
     padding-bottom: 50%
-  .icon
-    position relative
-    overflow: hidden
-    float: left
-    width:25%
-    height: 0
-    padding-bottom: 25%
-    .icon-img
-      position: absolute
-      left: 0
-      top: 0
-      right: 0
-      bottom: .44rem
-      box-sizing: border-box
-      padding: .1rem
-      .icon-img-content
-        height: 100%
-        display: block
-        margin: 0 auto
-    .icon-desc
-      position : absolute
-      left: 0
-      right: 0
-      bottom: 0
-      height: .44rem
-      line-height: .44rem
-      color: $darkTextColor
-      text-align :center
-      ellipsis()
+  .icons
+    margin-top: .1rem
+    .icon
+      position relative
+      overflow: hidden
+      float: left
+      width:25%
+      height: 0
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        left: 0
+        top: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding: .1rem
+        .icon-img-content
+          height: 100%
+          display: block
+          margin: 0 auto
+      .icon-desc
+        position : absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        color: $darkTextColor
+        text-align :center
+        ellipsis()
 </style>
---
+++ src/pages/home/components/recommend.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/201403/07/30b534b0de51ccac2906bcb6ba159988.jpg_200x200_2f08733b.jpg',
+          title: '长隆野生动物世界',
+          desc: '长隆野生动物世界'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1707/88/889ae02b07140066a3.img.jpg_200x200_d3b1427c.jpg',
+          title: '长隆欢乐世界',
+          desc: '长隆野欢乐世界'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1410/20/41d723daadc98045fe6b7283ba0af40c.jpg_200x200_5a05d5bc.jpg',
+          title: '碧水湾温泉度假村',
+          desc: '碧水湾温泉度假村'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang='stylus' scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-index: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      height: 1.7rem
+      width: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+</style>
